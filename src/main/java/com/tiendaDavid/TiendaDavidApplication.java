a769b03c096e3d60a7a920ff5e2c99d98bd1336b
David Sanchez <>
2023-06-20 19:51:33 -0600

    Se crea un nuevo repositorio de github y se agrega el portafolio inicial

---
+++ src/main/java/com/tiendaDavid/TiendaDavidApplication.java
@@ -0,0 +1,13 @@
+package com.tiendaDavid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaDavidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaDavidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiendaDavid/TiendaDavidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiendaDavid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaDavidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
